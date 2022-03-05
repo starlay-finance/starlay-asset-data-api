e27a66edaae6a6ee14069aa077f6ac25658b411c
atatur9 <momodarou2015@gmail.com>
2022-03-05 21:26:29 +0900

    initial commit

--- lib/starlay-asset-data-api-stack.ts
+++ lib/starlay-asset-data-api-stack.ts
@@ -1,16 +1,106 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import {
+  AuthorizationType,
+  DynamoDbDataSource,
+  FieldLogLevel,
+  GraphqlApi,
+  GraphqlApiProps,
+  MappingTemplate,
+  Schema,
+} from '@aws-cdk/aws-appsync';
+import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
+import {
+  Stack,
+  StackProps,
+  Construct,
+  Expiration,
+  Duration,
+  RemovalPolicy,
+} from '@aws-cdk/core';
+import { join } from 'path';
+import { Rule } from '@aws-cdk/aws-events';
+import { Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
+import { PolicyStatement, Effect } from '@aws-cdk/aws-iam';
+export const tableName = 'starlay-asset-data-table';
 
 export class StarlayAssetDataApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'StarlayAssetDataApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new Table(this, tableName, {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: tableName,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    const api = new GraphqlApi(this, 'graphqlapi', apiProps());
+    const ddbDs = new DynamoDbDataSource(this, 'ddbDataSource', {
+      api,
+      table,
+      readOnlyAccess: true,
+    });
+    api.createResolver({
+      typeName: 'Query',
+      fieldName: 'getAssetData',
+      dataSource: ddbDs,
+      requestMappingTemplate: MappingTemplate.fromFile(
+        join(__dirname, '..', 'schema', 'templates', 'getAssetData.request.vtl')
+      ),
+      responseMappingTemplate: MappingTemplate.fromFile(
+        join(
+          __dirname,
+          '..',
+          'schema',
+          'templates',
+          'getAssetData.response.vtl'
+        )
+      ),
+    });
+    const updatorFunction = new NodejsFunction(this, 'asset-data-updator', {
+      entry: 'src/incentive/index.ts',
+      handler: 'handler',
+      timeout: Duration.seconds(30),
+    });
+    updatorFunction.addToRolePolicy(
+      new PolicyStatement({
+        actions: ['dynamodb:PutItem'],
+        effect: Effect.ALLOW,
+        resources: [table.tableArn],
+      })
+    );
+    new Rule(this, 'updatorExecutionRule', {
+      schedule: Schedule.cron({
+        hour: '0',
+        minute: '0',
+      }),
+      targets: [
+        new LambdaFunction(updatorFunction, {
+          retryAttempts: 3,
+        }),
+      ],
+    });
   }
 }
+
+const apiProps = (): GraphqlApiProps => {
+  return {
+    xrayEnabled: true,
+    name: 'starlay-graphql-api',
+    logConfig: {
+      fieldLogLevel: FieldLogLevel.ALL,
+      excludeVerboseContent: false,
+    },
+    schema: Schema.fromAsset(join(__dirname, '..', 'schema/schema.graphql')),
+    authorizationConfig: {
+      defaultAuthorization: {
+        authorizationType: AuthorizationType.API_KEY,
+        apiKeyConfig: {
+          expires: Expiration.after(Duration.days(365)),
+          description: 'default',
+        },
+      },
+    },
+  };
+};
---
+++ src/incentive/index.ts
@@ -0,0 +1,69 @@
+import {
+  ReserveIncentiveWithFeedsResponse,
+  ReservesDataHumanized,
+  UiIncentiveDataProvider,
+  UiPoolDataProvider,
+} from '@starlay-finance/contract-helpers';
+import { ethers } from 'ethers';
+import { DynamoDB } from 'aws-sdk';
+import { tableName } from '../../lib/starlay-asset-data-api-stack';
+
+const ddbdc = new DynamoDB.DocumentClient();
+
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://rpc.astar.network:8545'
+);
+const uiPoolDataProviderAddress = '0x97Fc9e6aFB9d7A9C9898a2b6F97Da43EB5f56331';
+const incentiveDataProviderAddress =
+  '0x08ba69145938dD3CB0EE94c0D59EF6364059956B';
+const priceAggregatorAdapterAddress =
+  '0x21A097fE49F10E9cedD979eE46ADAF6eef87DE4b';
+const lendingPoolAddressProvider = '0x4c37A76Bf49c01f91E275d5257a228dad1b74EF9';
+
+interface DDBParam {
+  id: string; // id: date
+  timestamp: string;
+  poolData: ReservesDataHumanized;
+  reservesIncentives: ReserveIncentiveWithFeedsResponse[];
+}
+
+export async function handler(event: any, context: any): Promise<void> {
+  const uiPoolDataProvider = new UiPoolDataProvider({
+    provider,
+    uiPoolDataProviderAddress,
+  });
+  const uiIncentiveDataProvider = new UiIncentiveDataProvider({
+    provider,
+    incentiveDataProviderAddress,
+    priceAggregatorAdapterAddress,
+  });
+  const [poolData, reservesIncentives] = await Promise.all([
+    uiPoolDataProvider.getReservesHumanized(lendingPoolAddressProvider),
+    uiIncentiveDataProvider.getIncentivesDataWithPrice({
+      lendingPoolAddressProvider,
+    }),
+  ]);
+  const now = new Date();
+  const ddbParam: DDBParam = {
+    id: new Date(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate(),
+      now.getUTCHours()
+    ).toJSON(),
+    timestamp: Math.floor(now.getTime() / 1000).toString(),
+    poolData: poolData,
+    reservesIncentives: reservesIncentives,
+  };
+  const param: DynamoDB.DocumentClient.PutItemInput = {
+    TableName: tableName,
+    Item: ddbParam,
+  };
+  ddbdc.put(param, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
